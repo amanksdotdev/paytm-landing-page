309f41e8a8b3d2b81991239bd889ab3c02b2d5c2
Aman Singh <amanksofficial@gmail.com>
2022-08-21 22:24:21 +0530

    completed without interactive features

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bg_blue: "#00BAF2",
+        bg_darkblue: "#0F4A8A",
+        bg_gray: "#F5F7FA",
+        bg_darkgray: "#f6f6f6",
+        bg_lightblue: "#f0fbff",
+        btn_blue: "#01BBF2",
+        btn_darkblue: "#002970",
+        card_blue: "#66D6FF",
+        
+      },
+    },
+  },
+  plugins: [],
+};
